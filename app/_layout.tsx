c9ff30a0b511012277260e67992fd10f83a3d5e7
EHAB3x <ehabmah6969@gmail.com>
2024-12-02 14:42:29 +0200

    chat page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,96 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React from "react";
+import {
+  NavigationContainer,
+  NavigationIndependentTree,
+} from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import { EvilIcons, FontAwesome, FontAwesome6, Ionicons } from "@expo/vector-icons";
+import HomeScreen from "./(tabs)/index"; // Adjust the path as necessary
+import ChatPage from "./(tabs)/chat"; // Adjust the path as necessary
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const Tab = createBottomTabNavigator();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
-        <Stack.Screen name="+not-found" options={{headerShown : false}}/>
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <NavigationIndependentTree>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: "#EFEBF1",
+            tabBarInactiveTintColor: "#efebf175",
+            tabBarStyle: {
+              position: "absolute",
+              backgroundColor: "#3A0C87",
+              borderColor: "transparent",
+            },
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome name="moon-o" color="#EFEBF1" size={24} />
+              ),
+            }}
+          />
+
+          <Tab.Screen
+            name="Doctors"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome6 name="user-doctor" color="#EFEBF1" size={24} />
+              ),
+            }}
+          />
+
+          <Tab.Screen
+            name="Stats"
+            component={HomeScreen}
+            options={{
+              tabBarStyle:{
+                display:"flex"
+              },
+              tabBarIcon: ({ color, size }) => (
+                <EvilIcons name="gear" color="#EFEBF1" size={24} />
+              ),
+            }}
+          />
+
+          <Tab.Screen
+            name="Settings"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome6 name="user-doctor" color="#EFEBF1" size={24} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </NavigationIndependentTree>
   );
 }
+
+const styles = StyleSheet.create({
+  floatingButton: {
+    position: "absolute",
+    bottom: 60,
+    right: 20,
+    backgroundColor: "#3A0C87",
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    elevation: 5, // For Android shadow
+    shadowColor: "#000", // For iOS shadow
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
